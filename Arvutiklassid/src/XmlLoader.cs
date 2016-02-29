2bca27e871c9254441e2679f924dd639a91b03aa
viilveer <viilveer@hotmail.com>
2016-02-29 21:07:21 +0200

    xml validation

---
+++ Arvutiklassid/src/XmlLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Arvutiklassid.src
+{
+    class XmlLoader
+    {
+        public static XDocument SafeLoadXDocument(string xmlDocumentPath, string xsdSchemaPath)
+        {
+            XDocument xDocument = XDocument.Load(xmlDocumentPath);
+
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", xsdSchemaPath);
+
+
+            xDocument.Validate(schemas, (o, e) =>
+                {
+                    throw new Exception(e.Message);
+                }
+            );
+
+            return xDocument;
+        }
+    }
+}
